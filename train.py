bff8321467dcf32161ac76396d48c0c40c8bf945
Handre Bezuidenhout <handreb20@gmail.com>
2026-07-17 17:09:20 +0200

    Finished training loop

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from model import tbxrayCNN
+from data import get_dataloaders, get_transforms, all_images
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -12,6 +13,9 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 for epoch in range(15):
+    running_loss = 0.0
+    correct, total = 0, 0
+
     for images, labels in train_loader:
 
         images  = images.to(device)
@@ -21,7 +25,17 @@ for epoch in range(15):
 
         logits = model(images)
 
+        total += labels.size(0)
+        predictions = logits.argmax(dim=1)
+        correct += (predictions == labels).sum().item()
         loss = criterion(logits, labels)
         
         loss.backward()
         optimizer.step()
+
+        running_loss += loss.item()
+        
+
+    avg_loss = running_loss / len(train_loader)
+
+    print(f"Epoch: {epoch}  Avg Loss: {avg_loss} Accuracy: %{correct/total*100} ")
