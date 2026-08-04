a97ed4c6c405a8ec5d1200e94d7863476b9e6a23
Handre Bezuidenhout <handreb20@gmail.com>
2026-08-04 23:52:26 +0200

    Added fastapi point

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
+from fastapi import FastAPI, UploadFile
 from predict import predict
 
+app = FastAPI()
+
+@app.post("/predict")
+def predict_endpoint(file: UploadFile):
+    image_bytes = file.file.read()
+    tb_probability = predict(image_bytes)
+    return {"tb_probability": tb_probability}
